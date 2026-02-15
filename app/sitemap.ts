3acb700cb8ac0e0e6973e90bc0ad4d4bb07bf38b
Your Name <your@email.com>
2026-02-15 14:46:57 +0530

    Sync local code

---
+++ app/sitemap.ts
@@ -0,0 +1,33 @@
+import { MetadataRoute } from 'next'
+import { blogs } from './data/blogs'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+
+  const baseUrl = 'https://shubhamnarmadashivling.com'
+
+  const staticPages = [
+    '',
+    '/about',
+    '/blog',
+    '/product',
+    '/FAQ',
+    '/terms-conditions',
+    '/privacy-policy',
+    '/shipping-policy',
+    '/disclaimer',
+  ].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: route === '' ? 1 : 0.8,
+  }))
+
+  const blogPages = blogs.map((blog) => ({
+    url: `${baseUrl}/blog/${blog.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'daily' as const,
+    priority: 0.9,
+  }))
+
+  return [...staticPages, ...blogPages]
+}
